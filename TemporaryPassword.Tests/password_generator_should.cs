0e9bb182d7bea881242f96c325c4897be9f48813
Nieve <nievegoor@gmail.com>
2016-04-02 16:20:47 +0200

    added failing test for password creation

---
+++ TemporaryPassword.Tests/password_generator_should.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TemporaryPassword.Tests
+{
+    [TestFixture]
+    public class password_generator_should
+    {
+        IPasswordGenerator subject = new PasswordGenerator();
+
+        [Test]
+        public void create_password_for_int_id()
+        {
+            var id = 42;
+            var password = subject.Create(id);
+            Assert.NotNull(password);
+        }
+    }
+}
---
+++ TemporaryPassword/PasswordGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace TemporaryPassword
+{
+    public interface IPasswordGenerator
+    {
+        string Create(int id);
+    }
+
+    public class PasswordGenerator : IPasswordGenerator
+    {
+        public string Create(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
